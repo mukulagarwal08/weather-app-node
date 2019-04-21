1a7b1a45a3ad00e9eb237aa9d7da0ad583fed5ff
Mukul Agarwal <mukul@Mukuls-MacBook-Pro.local>
2019-04-21 23:35:45 +0530

    Remove unecessary console.log call

--- public/js/app.js
+++ public/js/app.js
@@ -1,12 +1,9 @@
-console.log("client side javascript is loaded")
 
 // fetch('http://puzzle.mead.io/puzzle').then((response)=>{
 //     response.json().then((data)=>{
 //         console.log(data)
 //     })
 // })
-
-
 
 const weatherForm = document.querySelector('form')
 const search = document.querySelector('input')
